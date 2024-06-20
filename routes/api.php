443ef3bed92cf0bf990ba907c96dceddab360a6a
diegovega223 <diegovegaganachipi@gmail.com>
2024-06-20 18:22:09 -0300

    add docker-compose and welcome.blade.php

--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,13 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::prefix('v1')->group(function ()
 {
     Route::post('/user',[UserController::class,"register"]);
     Route::get('/validate',[UserController::class,"validarToken"])->middleware('auth:api');
     Route::get('/logout',[UserController::class,"logout"])->middleware('auth:api');
-
-
 });
